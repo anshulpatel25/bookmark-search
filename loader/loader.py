d4a2b9e44d9dd316e336547c4180c5762cbf200a
Anshul Patel <anshulpatel25@gmail.com>
2020-04-25 10:19:06 +0530

    Added User Agent and CPU args.

--- loader/loader.py
+++ loader/loader.py
@@ -18,7 +18,7 @@ from elasticsearch import Elasticsearch
 logging.basicConfig(level=logging.INFO)
 
 
-def ingest_data(bookmark_url, host, port, index, db_location):
+def ingest_data(bookmark_url, host, port, index, db_location, user_agent):
 
     url_hash = hashlib.sha256(bytes(bookmark_url, "utf-8")).hexdigest()
 
@@ -35,31 +35,45 @@ def ingest_data(bookmark_url, host, port, index, db_location):
 
     if not exists[0]:
         try:
-            raw_resp = requests.get(bookmark_url, timeout=10)
 
-            logging.info("Transforming %s ...", bookmark_url)
-            raw_text = bs(raw_resp.content, "html.parser").get_text(strip=True)
-            processed_data = re.sub(r"[\r\t\n]", "", re.sub(r"[^\w\s]", "", raw_text))
-            website = "{uri.netloc}".format(uri=urlparse(bookmark_url))
-            logging.info("Ingesting %s ...", bookmark_url)
-            ingestion_client.index(
-                index=index,
-                body={
+            headers = {"User-Agent": user_agent}
+            raw_resp = requests.get(bookmark_url, timeout=10, headers=headers)
+
+            if raw_resp.status_code == 200:
+                logging.info("Transforming %s ...", bookmark_url)
+                raw_text = bs(raw_resp.content, "html.parser").get_text(strip=True)
+                processed_data = re.sub(
+                    r"[\r\t\n]", "", re.sub(r"[^\w\s]", "", raw_text)
+                )
+                website = "{uri.netloc}".format(uri=urlparse(bookmark_url))
+                logging.info("Ingesting %s ...", bookmark_url)
+                ingestion_client.index(
+                    index=index,
+                    body={
+                        "url": bookmark_url,
+                        "content": processed_data,
+                        "website": website,
+                    },
+                )
+                logging.info("Successfully processed %s", bookmark_url)
+                cursor.execute("insert into PROCESS_STATUS values (?)", (url_hash,))
+                connection.commit()
+                return {"status": "success", "url": bookmark_url, "reason": None}
+            else:
+                logging.info("Unable to Process %s", bookmark_url)
+                return {
+                    "status": "fail",
+                    "reason": str(raw_resp.status_code),
                     "url": bookmark_url,
-                    "content": processed_data,
-                    "website": website,
-                },
-            )
-            logging.info("Successfully processed %s", bookmark_url)
+                }
 
-            cursor.execute("insert into PROCESS_STATUS values (?)", (url_hash,))
-            connection.commit()
-            return {"status": "success", "url": bookmark_url, "reason": None}
         except Exception as error:
             logging.info("Unable to Process %s", bookmark_url)
             return {"status": "fail", "reason": str(error), "url": bookmark_url}
+
         finally:
             connection.close()
+
     else:
         logging.info("Skipping %s as it exists", bookmark_url)
 
@@ -98,16 +112,21 @@ def initialize_db(db_location):
     logging.info("Database initialized")
 
 
-def process(location, host, port, index, db_location):
+def process(location, host, port, index, db_location, cpu, user_agent):
 
     bookmarks_tree = get_bookmark_tree(location)
     bookmarks_url_list = get_bookmark_urls(bookmarks_tree)
 
-    with Pool(4) as mp:
+    with Pool(cpu) as mp:
         # results = mp.map(ingest_data, bookmarks_url_list)
         results = mp.map(
             partial(
-                ingest_data, host=host, port=port, index=index, db_location=db_location
+                ingest_data,
+                host=host,
+                port=port,
+                index=index,
+                db_location=db_location,
+                user_agent=user_agent,
             ),
             bookmarks_url_list,
         )
@@ -168,7 +187,26 @@ def main():
         help="Location to SqliteDB, it will be created if not provided",
     )
 
+    parser.add_option(
+        "-c", "--cpu", dest="cpu", default=4, type="int", help="Number of CPUs",
+    )
+
+    parser.add_option(
+        "-a",
+        "--agent",
+        dest="agent",
+        default="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36",
+        type="string",
+        help="User-Agent to use while scraping websites",
+    )
+
     (options, args) = parser.parse_args()
+
+    required_flags = ["location", "server", "port", "index", "database_location"]
+
+    for flag in required_flags:
+        if options.__dict__[flag] is None:
+            parser.error("Parameter --%s required" % flag)
 
     initialize_db(options.database_location)
 
@@ -178,6 +216,8 @@ def main():
         port=options.port,
         index=options.index,
         db_location=options.database_location,
+        cpu=options.cpu,
+        user_agent=options.agent,
     )
 
 
